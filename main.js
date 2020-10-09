6fe5c49cd7565979e710c68fc79068cdf43e7bf8
Senura Ratnayake <kolithaur@gmail.com>
2020-10-09 17:26:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+var mybutton = document.getElementById("myBtn");
+
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+        } else {
+           mybutton.style.display = "none";
+       }
+     }
+
+
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+     }
+
+
+    var mybutton = document.getElementById("myBtn");
+
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+        } else {
+           mybutton.style.display = "none";
+       }
+     }
+
+
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+     }
+
+  function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+     }
+
+/* Set the width of the side navigation to 0 */
+    function closeNav() {
+     document.getElementById("mySidenav").style.width = "0";
+     }
